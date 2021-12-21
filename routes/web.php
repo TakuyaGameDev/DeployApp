ebd7ceb7b1f84bf51a44a0969bebadc24e9cc8b0
TakuyaGameDev <takuya-uverokrock@outlook.jp>
2021-12-21 14:54:48 +0900

    web.php second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-namespace Laravel\Ui;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
